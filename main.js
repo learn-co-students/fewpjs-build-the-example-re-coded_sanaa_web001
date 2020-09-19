4207ff05a261e5cef08dafe05fe5cfb1d840edc3
hanan7121993 <hanan7121993@gmail.com>
2020-09-19 06:04:11 +0000

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+var hash = {
+  EMPTY_HEART: FULL_HEART,
+  FULL_HEART: EMPTY_HEART
+}
+var modal = document.querySelector(".modal")
+var liker = document.querySelector(".like")
+// Your JavaScript code goes here!
 
+// liker.addEventListener("click", (event) => {
+//   mimicServerCall(function (url) {
+//   .then((result => event.target.innerText = hash[event.target.innerText];
+//   .catch((error => {
+//     modal.classList.remove("hidden");
+//     })
+//   })
+// });
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall("bogusUrl")
+    //OR: mimicServerCall("bogusUrl", {forceFailure: true})
+    .then(function (serverMessage) {
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function (error) {
+      // Basic
+      // alert("Something went wrong!");
+      // or....
+      document.getElementById("modal").className = "";
+    });
+}
 
 
 
